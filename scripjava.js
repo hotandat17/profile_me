8b2aca5cb461c77b76b52be9b163eef35388a9d0
hotandat17 <hotandat17@gmail.com>
2024-05-10 23:15:03 +0700

    Profile

---
+++ scripjava.js
@@ -0,0 +1,24 @@
+document.getElementById('openModal').addEventListener('click', function () {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function () {
+    document.getElementById('myModal').style.display = 'none';
+});
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
